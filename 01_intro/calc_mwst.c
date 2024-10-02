4f0cf9d5ae6ff7cc9d2bad53fd6e491c39c56253
Dennis Grewe <dennis.grewe@hs-esslingen.de>
2024-10-02 08:04:15 +0200

    incl. lecture comments in mwst example
    Signed-off-by: Dennis Grewe <dennis.grewe@hs-esslingen.de>

--- 01_intro/calc_mwst.c
+++ 01_intro/calc_mwst.c
@@ -7,10 +7,16 @@
 /* Hauptprogramm Mehrwertsteuer */
 int main(void) {
 
+    // Variablen für eingegebenen Betrag und Gesamtbetrag festlegen
     float betrag, ges_betrag;
+    // Benutzer um Eingabe "Betrag" bitten
     printf("Nettobetrag in Euro: ");
+    // Eingabe vom Benutzer lesen mittels scanf(), beachten Sie "&" Symbol zwingend notwendig
     scanf("%f", &betrag);
+    // Berechnen der Mehrwertsteuer: Betrag * 1.19 = Gesamtbetrag
+    // Beachten Sie: Zuweisungsoperation ist rechtsassoziativ, erfolgt von rechts nach links.
     ges_betrag = betrag * MWST;
+    // Ausgabe Gesamtbetrag auf Konsole mittels printf()
     printf("Der Gesamtbetrag ist %f\n", ges_betrag);
 
     return 0;
